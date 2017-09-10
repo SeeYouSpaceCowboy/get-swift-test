23b028c2fa0924057fcd838866968f0d673e6a43
Mohammed Chisti <93mchisti@gmail.com>
2017-09-10 11:52:01 -0400

    Adding images

---
+++ javascripts/side-nav.js
@@ -0,0 +1,9 @@
+function openSideNav() {
+    document.getElementById("sideNav").classList.add('side-nav-open');
+    document.getElementById("sideNav").style.display = 'block'
+}
+
+function closeSideNav() {
+    document.getElementById("sideNav").classList.remove('side-nav-open');
+    document.getElementById("sideNav").style.display = 'none'
+}
